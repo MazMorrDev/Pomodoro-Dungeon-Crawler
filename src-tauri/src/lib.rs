98101ff6879444554039bd163223a9c42de832a7
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-05-04 00:02:29 -0400

    feat: make a function that works as an example of how to make the integration between tauri and anstro

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,3 +1,22 @@
+use tauri::{AppHandle, Emitter};
+use serde::Serialize;
+
+#[derive(Clone, Serialize)]
+struct ProgressPayload {
+    percentage: u8,
+}
+
+#[tauri::command]
+async fn start_work(app: AppHandle) -> Result<String, String> {
+    for i in 0..=10 {
+        tokio::time::sleep(std::time::Duration::from_millis(1000)).await;
+        let pct = i * 10;
+        app.emit("progress-update", ProgressPayload { percentage: pct })
+            .map_err(|e| e.to_string())?;
+    }
+    Ok("Listo".into())
+}
+
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
@@ -11,6 +30,7 @@ pub fn run() {
             }
             Ok(())
         })
+        .invoke_handler(tauri::generate_handler![start_work])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,5 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
-const MY_STRING2: &str = "Esto es una cadena de texto constante";
-
 fn main() {
-    println!("{MY_STRING2}");
     app_lib::run();
 }
