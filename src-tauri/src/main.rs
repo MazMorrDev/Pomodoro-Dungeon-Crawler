490cafc128760b32e402868a145b31ff776c2368
Marco Antonio Romero Albanez <122071151+MazMorrDev@users.noreply.github.com>
2026-05-03 20:56:11 -0400

    add global css

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,6 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+const MY_STRING2: &str = "Esto es una cadena de texto constante";
 
 fn main() {
-  app_lib::run();
+    println!("{MY_STRING2}");
+    app_lib::run();
 }
